917dc8d454ac04040a5bfe3828c8cc3382a3d989
Arelera <arelcode@gmail.com>
2024-08-18 20:31:20 +0300

    add formik and yup packages, add register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,5 +1,42 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import { useNavigate } from 'react-router-dom'
+import * as yup from 'yup'
 import MainLayout from '../layouts/MainLayout'
 
 export default function RegisterPage() {
-  return <MainLayout>Register Page</MainLayout>
+  const navigate = useNavigate()
+
+  return (
+    <MainLayout>
+      Register Page
+      <div>
+        <Formik
+          initialValues={{ name: '', cv: '', resume: '' }}
+          validationSchema={schema}
+          onSubmit={() => {
+            navigate('/dashboard')
+          }}
+        >
+          <Form>
+            <div>
+              <label htmlFor="firstName">Name</label>
+              <Field id="name" name="name" />
+              <ErrorMessage name="name" />
+            </div>
+            <div>
+              <label htmlFor="cv">CV</label>
+              <Field id="cv" name="cv" as="textarea" />
+              <ErrorMessage name="cv" />
+            </div>
+            <button type="submit">Register</button>
+          </Form>
+        </Formik>
+      </div>
+    </MainLayout>
+  )
 }
+
+const schema = yup.object({
+  name: yup.string().required(),
+  cv: yup.string().required(),
+})
