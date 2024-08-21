ccd7d674d281dfaf2e71c8a0591102bccc6ff093
Arelera <arelcode@gmail.com>
2024-08-21 21:26:51 +0300

    add welcome text to the homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,10 @@ import DashboardLayout from '../layouts/DashboardLayout'
 export default function HomePage() {
   return (
     <DashboardLayout crumbs={[{ label: 'Home', href: '/' }]}>
-      Home Page
+      <div className="position-relative text-center my-6">
+        <h1 className="text-white">Welcome to ILA Case!</h1>
+        <p className="text-white">Please login to use the dashboard.</p>
+      </div>
     </DashboardLayout>
   )
 }
